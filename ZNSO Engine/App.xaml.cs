ddc3621512b4b5f9673e1be2dff4898b81c1870c
CristKenydy <znsocorporation@outlook.com>
2025-05-21 15:21:19 +0800

    Add files via upload

---
+++ ZNSO Engine/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using ZNSO_Engine.Editors;
+
+namespace ZNSO_Engine;
+
+public partial class App : Application
+{
+    MainWindow mainWindow;
+    MainEditor editor;
+    public App()
+    {
+        mainWindow = new MainWindow();
+        if (mainWindow.ShowDialog() == false)
+        {
+            Current.Shutdown();
+        }
+        else if (mainWindow.ShowDialog() == true)
+        {
+            editor = new MainEditor();
+            editor.Show();
+        }
+    }
+}
+
